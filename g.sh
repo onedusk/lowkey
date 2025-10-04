f556c26e8408d21095cf4d74caefcf912728a4e0
onedusk <one@dusk-labs.com>
2025-10-04 07:48:17 -0400

    first commit

--- g.sh
+++ g.sh
@@ -5,5 +5,5 @@ git init;
 git add -A;
 git commit -m "first commit";
 git branch -M main;
-git remote add origin https://github.com/onedusk/lowkey.git;
+git remote add origin git@github.com:onedusk/lowkey.git;
 git push -u origin main;
