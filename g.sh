470dc453c041fcc13cc128d7ff7ea2430fbe6060
onedusk <one@dusk-labs.com>
2025-10-04 07:51:45 -0400

    added .github

--- g.sh
+++ g.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # echo "# lowkey" >> README.md
-git init;
+# git init;
 git add -A;
-git commit -m "first commit";
-git branch -M main;
-git remote add origin git@github.com:onedusk/lowkey.git;
+git commit -m "added .github";
+# git branch -M main;
+# git remote add origin git@github.com:onedusk/lowkey.git;
 git push -u origin main;
